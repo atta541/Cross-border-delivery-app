96604aaa5c98500805e57560b43e6da4f376fb5e
Atta ur rehman <114917589+atta541@users.noreply.github.com>
2025-02-20 19:25:45 +0500

    Update firebaseConfig.js

--- delivajoymobile/firebaseConfig.js
+++ delivajoymobile/firebaseConfig.js
@@ -3,12 +3,12 @@ import { getDatabase } from 'firebase/database';
 import { getFunctions } from 'firebase/functions'; 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDvFndRijNdjExZCRe7RbgBobq962FU3kI",
+  apiKey: "A*************************kI",
   authDomain: "YOUR_AUTH_DOMAIN",
   projectId: "delivajoymobile-7c40c",
   storageBucket: "YOUR_STORAGE_BUCKET",
   messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "1:38106251938:android:f642bd7798584cdfaf0dfb",
+  appId: "1:381062******************fb",
 };
 
 const app = initializeApp(firebaseConfig);
